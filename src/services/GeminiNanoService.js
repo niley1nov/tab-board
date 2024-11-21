37c2a3bdd3078c16288a79664da9d6b35a5737c7
Nilay Sharma <yalin.yhb@gmail.com>
2024-11-21 22:19:41 +0530

    nano setup

--- src/services/GeminiNanoService.js
+++ src/services/GeminiNanoService.js
@@ -1,7 +1,16 @@
 import AIService from './AIService';
+import { getPrompts } from "./AIConfigData.js";
 
 export default class GeminiNanoService extends AIService {
     async callModel(prompt) {
-        console.log('callModel', prompt);
+        try {
+			let chatSession = await window.ai.languageModel.create({
+				systemPrompt: getPrompts("system_prompt")
+			});
+			let result = await chatSession.prompt(prompt);
+			return result;
+		} catch(error) {
+			console.error(error);
+		}
     }
 }
